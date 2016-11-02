6a9f94a7e4113eec6dac91b009c31426f485d568
radamantisdh <jpablo.venegass@gmail.com>
2016-11-02 15:34:18 -0300

    Update db.class.php

--- includes/db.class.php
+++ includes/db.class.php
@@ -1,16 +1,16 @@
 <?php
  	class DB {
-	public $host = "localhost";
-	public $user = "root";
-	public $pwd  = "admin";
-	public $db = "irso"; //hace referencia a la bd de mysql
-	public $consulta;
+	public $host = "10.3.184.21"; //host
+	public $user = "postgres"; //user bd postgress
+	public $pwd  = "seremi9"; //pass db postgress
+	public $db = "IRSO"; //este esta haciendo referencia a la bd de mysql por lo q en local funciona (haciendo referencia a alguna db q exista)  
+ 	public $consulta;
 
 	public function conectar()
 	{
 		mysql_connect($this->host, $this->user, $this->pwd);
-		mysql_select_db($this->db) or die("Fallo la Conexión");
-		pg_connect("host=localhost port=5432 dbname=IRSO user=postgres password=postgresql"); //abro conexion a postgres
+		mysql_select_db($this->db) or die("Fallo la Conexión"); //este error esta arrojando
+		pg_connect("host=10.3.184.21 port=5432 dbname=IRSO user=postgres password=seremi9"); 
 	}
 
 	public function desconectar() 
